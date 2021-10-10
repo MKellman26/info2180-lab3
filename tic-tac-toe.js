b7c9ca3da302ad20cbce17baca9b2a943e965266
MKellman26 <57738618+MKellman26@users.noreply.github.com>
2021-10-10 01:32:03 -0500

    Completed "Add an X or O to a square when clicked"

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,7 +3,19 @@
 window.onload = function(){
     var brd= document.getElementById('board');
     var sqrs= brd.getElementsByTagName('div');
+    var status = []
     for (var i = 0; i< sqrs.length; i+=1){
         sqrs[i].setAttribute('class','square');
-    }
+        sqrs[i].addEventListener('click', function(e) {
+            if (status.length%2 == 0){
+                e.target.classList.add('X');
+                e.target.innerHTML = "X";
+                status.push("X");
+            } else {
+                e.target.classList.add('O');
+                e.target.innerHTML = "O";
+                status.push("O");
+            }
+        });
+}
 }
